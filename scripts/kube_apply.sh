52a26faffba7ea7ee8f2915d64884852ed018e20
aanorlondo <khaled.khebbeb@gmail.com>
2023-05-21 11:57:02 +0200

    repo: moved scripts under kube dir

--- scripts/kube_apply.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Apply Kubernetes configurations
-kubectl apply -f ../kubernetes/external-configs/clipboard-external-config.yaml
-kubectl apply -f ../kubernetes//secrets/clipboard-secrets.yaml
-kubectl apply -f ../kubernetes//services/clipboard-mongodb.yaml
-kubectl apply -f ../kubernetes//services/clipboard-webapp.yaml
