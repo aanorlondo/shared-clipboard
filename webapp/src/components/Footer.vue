eb8f8010aae29cc96cf4ec9d6d8014972c7a0ee4
aanorlondo <khaled.khebbeb@gmail.com>
2023-05-21 19:10:48 +0200

    feat: responsive ok

--- webapp/src/components/Footer.vue
+++ webapp/src/components/Footer.vue
@@ -2,14 +2,14 @@
     <footer id="footer">
         <div class="footer-content">
             <a href="https://github.com/aanorlondo/shared-clipboard" target="_blank" rel="noopener noreferrer">
-                <img src="../assets/media/github_logo.png" alt="GitHub Logo" class="github-logo">
+                <img src="../assets/media/github_logo.png" alt="GitHub Logo" id="github-logo">
             </a>
             <p class="copy">&copy; 2023 Khaled Khebbeb. All rights reserved.</p>
             <p class="built-with">
-                <img src="../assets/media/vuejs_logo.png" alt="Vue.Js" class="vuejs-logo">
+                <img src="../assets/media/vuejs_logo.png" alt="Vue.Js" id="vuejs-logo">
             </p>
             <p class="served-with">
-                <img src="../assets/media/nodejs_logo.png" alt="Node.Js" class="nodejs-logo">
+                <img src="../assets/media/nodejs_logo.png" alt="Node.Js" id="nodejs-logo">
             </p>
         </div>
     </footer>
