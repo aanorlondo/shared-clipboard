799a354a8ff1b69522c13b70b478fe1150535a9e
aanorlondo <khaled.khebbeb@gmail.com>
2023-05-21 13:57:34 +0200

    feat: added tech logos in footer

--- webapp/src/components/Footer.vue
+++ webapp/src/components/Footer.vue
@@ -5,6 +5,12 @@
                 <img src="../assets/media/github_logo.png" alt="GitHub Logo" class="github-logo">
             </a>
             <p class="copy">&copy; 2023 Khaled Khebbeb. All rights reserved.</p>
+            <p class="built-with">
+                <img src="../assets/media/vuejs_logo.png" alt="Vue.Js" class="vuejs-logo">
+            </p>
+            <p class="served-with">
+                <img src="../assets/media/nodejs_logo.png" alt="Node.Js" class="nodejs-logo">
+            </p>
         </div>
     </footer>
 </template>
