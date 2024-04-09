b6d3622d49c3f7643c4d944731e48e4173bba4d5
aanorlondo <khaled.khebbeb@gmail.com>
2024-04-09 19:25:04 +0200

    fix: build path

--- local/docker_build.sh
+++ local/docker_build.sh
@@ -5,12 +5,10 @@ docker rmi \
     negan/clipboard-mongo:local \
     negan/clipboard-webapp:local
 
-
 # build the mongo db
 docker build -t negan/clipboard-mongo:local ../mongo-init
 docker push negan/clipboard-mongo:local
 
-
 # build the webapp
-docker build -t negan/clipboard-webapp:local ../backend-app
+docker build -t negan/clipboard-webapp:local ../webapp
 docker push negan/clipboard-webapp:local
